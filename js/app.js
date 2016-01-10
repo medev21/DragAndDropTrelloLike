1275db820c254508521a29f272178cc00d703ea4
Martin Ben <mebenav88@gmail.com>
2016-01-10 14:06:23 -0500

    add drag and drop across the list, need to fix sortable within list

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-// $('.list-sortable').sortable();
-
+// sortables
 $( ".list-sortable" ).sortable();
 $( ".list-sortable" ).disableSelection();
+
+$( ".list" ).sortable();
+$( ".list" ).disableSelection();
+
+// drag and drop
+$(".card").draggable({
+  revert: "invalid",
+  snap: "true",
+  snapMode: "inner"
+});
+
+$(".list").droppable({
+  revert: "invalid",
+  // accept: '.card',
+  drop: function(event, ui){
+    ui.helper.css('top','');
+    ui.helper.css('left','')
+    $(this).find('.bottom').prepend(ui.helper);
+  }
+});
