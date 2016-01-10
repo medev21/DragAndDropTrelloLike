1010310e5daae0f7b8977b3c0c861314cf1e1029
Martin Ben <mebenav88@gmail.com>
2016-01-10 15:15:03 -0500

    add spacing between cards and lists, still need to debug - cards snap to the top in drag and drop

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 // sortables
+
 $( ".list-sortable, .list" ).sortable();
 $( ".list-sortable, .list" ).disableSelection();
+
+// $( ".list-sortable" ).sortable();
+// $( ".list-sortable" ).disableSelection();
+//
+// $( ".bottom" ).sortable();
+// $( ".bottom" ).disableSelection();
+
 $(".list").sortable({
   cancel: 'header'
 });//this exclude the header tag from being sorted!
+
 ///////end of sortables////////////
 
+
 ////// drag and drop////////
+
 $(".card").draggable({
   revert: "invalid",
   snap: "true",
@@ -15,11 +26,15 @@ $(".card").draggable({
 
 $(".list").droppable({
   revert: "invalid",
-  // accept: '.card',
+  accept: '.card',
   drop: function(event, ui){
     ui.helper.css('top','');
-    ui.helper.css('left','')
+    ui.helper.css('left','');
     $(this).find('.bottom').prepend(ui.helper);
   }
 });
+
 ///////////end of drag and drop////////////////
+
+
+// $( ".list" ).sortable();
